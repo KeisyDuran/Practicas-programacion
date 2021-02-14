6fe8a4f679aad3cab8a2115502eb477d136ef8d4
Keisy.Duran <KeisyDuran@users.noreply.github.com>
2021-02-14 07:21:04 -0400

    .

--- practica03/ejercicio1/Program.cs
+++ practica03/ejercicio1/Program.cs
@@ -6,7 +6,21 @@ namespace ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              int num=1, cont=0;
+            float suma=0;
+
+            while (num!=0)
+            {
+                Console.Write("Introduzca un numero: ");
+                num = int.Parse (Console.ReadLine());
+                suma=suma+num;
+                if (num!=0)
+                {
+                  cont=cont+1;                   
+                }
+            }
+                 Console.WriteLine("La sumatoria es: {0}", suma);
+                 Console.WriteLine("La cantidad de numeros ingresados es: {0}", cont);
         }
     }
 }
