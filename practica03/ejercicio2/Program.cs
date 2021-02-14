a3e5561a32191e3d2c2348d514901639d02a0956
Keisy.Duran <KeisyDuran@users.noreply.github.com>
2021-02-14 08:34:12 -0400

    .

--- practica03/ejercicio2/Program.cs
+++ practica03/ejercicio2/Program.cs
@@ -6,7 +6,15 @@ namespace ejercicio2
     {
         static void Main(string[] args)
         {
-            
+            int a, b;
+            b = 1;
+
+           do
+           {
+            a = b++;
+            Console.Write( "\n" + a);   
+           } while (a!=10);
+
         }
     }
 }
