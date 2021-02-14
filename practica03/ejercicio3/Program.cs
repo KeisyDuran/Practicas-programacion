c265e142ed505a596262c02eb7ab4ac7b3ed9e2e
Keisy.Duran <KeisyDuran@users.noreply.github.com>
2021-02-14 09:01:38 -0400

    .

---
+++ practica03/ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int contador = 10*10;
+           while (contador <= 120)
+          {
+            if (contador % 2 == 0)
+            {
+                Console.WriteLine(contador);
+            }
+            contador += 1;
+          }
+        }
+    }
+}
