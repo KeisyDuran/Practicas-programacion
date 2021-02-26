091076d8135fadd5cda6e7b3c40cefcea590108e
Keisy.Duran <KeisyDuran@users.noreply.github.com>
2021-02-25 23:22:07 -0400

    .

--- ejercicios_arrays_clases/ejercicio2/Program.cs
+++ ejercicios_arrays_clases/ejercicio2/Program.cs
@@ -6,7 +6,23 @@ namespace ejercicio2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+//2.	Un programa que pida al usuario 5 números reales (pista: necesitarás un array de "float") 
+//y luego los muestre en el orden contrario al que se introdujeron.           
+            float[] numero = new float[5];
+ 
+           Console.Write("\nTeclee el primer número:    ");
+            numero[0] = Convert.ToSingle(Console.ReadLine());
+           Console.Write("\nTeclee el segundo número:    ");
+            numero[1] = Convert.ToSingle(Console.ReadLine());
+           Console.Write("\nTeclee el tercero número:    ");
+            numero[2] = Convert.ToSingle(Console.ReadLine());
+           Console.Write("\nTeclee el cuarto número:    ");
+            numero[3] = Convert.ToSingle(Console.ReadLine());
+           Console.Write("\nTeclee el quinto número:    ");
+            numero[4] = Convert.ToSingle(Console.ReadLine());
+ 
+          Console.WriteLine("\n\nLos números tecleados alrrevés son:  {4}, {3}, {2}, {1}, {0}", numero[0], numero[1], numero[2], numero[3], numero[4]);
+          Console.ReadLine();
         }
     }
 }
