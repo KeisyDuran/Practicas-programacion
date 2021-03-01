d82837027c2a9ea03328d12d4898d17a8da1550d
Keisy.Duran <KeisyDuran@users.noreply.github.com>
2021-02-28 21:01:38 -0400

    .

--- ejercicios_arrays_clases/ejercicio8/Program.cs
+++ ejercicios_arrays_clases/ejercicio8/Program.cs
@@ -6,9 +6,55 @@ namespace ejercicio8
     {
         static void Main(string[] args)
         {
-            
-            
+            int f;
+            int may;
+            int men;
+	       float[] alturas = new float[5];
+	       float suma;
+	       float promedio;
 
+	       for (f = 0; f < 5; f++)
+	       {
+		     Console.Write("Ingrese la altura de la persona: ");
+		     string tempVar = Console.ReadLine();
+		      if (tempVar != null)
+		       {
+			     alturas[f] = float.Parse(tempVar);
+		        }
+	        }
+	      suma = 0F;
+	        for (f = 0; f < 5; f++)
+	        {
+		       suma = suma + alturas[f];
+	        }
+	      promedio = suma / 5;
+	      Console.Write("Promedio de alturas: ");
+          Console.Write("{0:f2}",promedio);
+	      Console.Write("\n");
+
+	     may = 0;
+	     men = 0;
+	      for (f = 0; f < 5; f++)
+	      {
+		    if (alturas[f] > promedio)
+		    {
+			 may++;
+		    }
+		    else
+		    {
+			   if (alturas[f] < promedio)
+			   {
+				 men++;
+			   }
+		    }
+	       }
+
+	      Console.Write("Cantidad de personas mayores al promedio: ");
+	      Console.Write("{0:D}",may);
+	      Console.Write("\n");
+	      Console.Write("Cantidad de personas menores al promedio: ");
+	      Console.Write("{0:D}",men);
+	      Console.ReadKey(true);  
         }
     }
 }
