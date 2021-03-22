eed8c00c6b68f65ceee62e5fd9ab869165ae2fd0
Keisy.Duran <77956724+KeisyDuran@users.noreply.github.com>
2021-03-22 15:48:29 -0400

    .

--- practica06/ejercicio1/Program.cs
+++ practica06/ejercicio1/Program.cs
@@ -1,7 +1,41 @@
 ﻿using System;
 
 namespace ejercicio1
-{
+{   public class persona 
+    {
+      private int Cedula;
+      private string Nombre;
+      private string Apellido;
+      private int Edad;
+
+      public int cedula 
+      {
+          get { return cedula; }
+          set { cedula = value; }
+      }
+      public string Nombre 
+      {
+          get { return Nombre; }
+          set { Nombre = value; }
+      }
+      public string Apellido 
+      {
+          get { return Apellido; }
+          set { Apellido = value; }
+      }
+      public int Edad 
+      {
+          get { return Edad; }
+          set { Edad = value; }
+      }
+      public persona (int pcedula, string pnombre, string papellido, int pedad)
+      {
+        Cedula = pcedula;
+        Nombre = pnombre;
+        Apellido = papellido;
+        Edad = pedad;   
+      }
+    }
     class Program
     {
         static void Main(string[] args)
