9482362dd1897b264829fe0c74341bc8a935afd9
Keisy.Duran <77956724+KeisyDuran@users.noreply.github.com>
2021-03-22 01:13:45 -0400

    .

---
+++ practica06/ejercicio1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ejercicio1
+{
+     public class persona
+    {
+        public int cedula { get; set; }
+         public string Nombre { get; set; }
+         public string Apellido { get; set; }
+         public int Edad { get; set; }
+         public persona ()
+        {   
+            Console.WriteLine("Su Cedula es: " + cedula);
+            Console.WriteLine("Su nombre es: " + Nombre);
+            Console.WriteLine("Su Apellido es: " + Apellido);
+            Console.WriteLine("Su Edad es: " + Edad);
+        }
+        public int metodo (int sueldo)  
+        {
+            return Console.Write("Su sueldo es de: ");
+        } 
+    }
+   
+    public class profesor : persona
+    {
+        public profesor ()
+        {
+            Console.WriteLine("Su Cedula es: "+ cedula);
+            Console.WriteLine("Su nombre es: " + Nombre);
+            Console.WriteLine("Su Apellido es: " + Apellido);
+            Console.WriteLine("Su Edad es: " + Edad);
+            Console.WriteLine("Su Sueldo es de: " + Sueldo);
+        }
+        public int metodo (int sueldo)  
+        {
+            return base.metodo (Console.Write("Su sueldo es de: ")) + 25000;
+        } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          profesor profesor = new profesor ();
+          persona persona = new persona ();
+
+          Console.WriteLine("Clase Persona"); 
+          Console.Write("Escriba su Cedula: ");
+          persona.cedula = int.Parse(Console.ReadLine());
+          Console.Write("Esciba su Nombre: ");
+          persona.Nombre = Console.ReadLine();
+          Console.Write("Escriba su Apellido: ");
+          persona.Apellido = Console.ReadLine();
+          Console.Write("Escriba su Edad: ");
+          persona.Edad = int.Parse(Console.ReadLine());
+          Console.WriteLine("-----------------------------------------");   
+          Console.WriteLine("Clase Profesor");   
+          Console.Write("Escriba su Cedula: ");
+          profesor.cedula = int.Parse(Console.ReadLine());
+          Console.Write("Esciba su Nombre: ");
+          profesor.Nombre = Console.ReadLine();
+          Console.Write("Escriba su Apellido: ");
+          profesor.Apellido = Console.ReadLine();
+          Console.Write("Escriba su Edad: ");
+          profesor.Edad = int.Parse(Console.ReadLine()); 
+          Console.Write(profesor.metodo);  
+           
+           
+           
+          Console.ReadLine();     
+        }
+    }
+   
+}
---
+++ practica06/ejercicio3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ejercicio3
+{   class claseA
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+
+        public void infoA()
+        {
+            Console.WriteLine("Su Nombre es " + nombre);
+            Console.WriteLine("Su Apellido es " + apellido);
+        }
+    }
+    class ClaseB:claseA
+    {
+        public int edad { get; set; }
+        public int sueldo { get; set; }
+
+        public void infoB()
+        {
+            Console.WriteLine("Su edad es: " + edad);
+            Console.WriteLine("Su sueldo es: " + sueldo);
+
+        }
+    }
+    class ClaseC:ClaseB
+    {
+        public string NombrePapa { get; set; }
+        public string NombreMama { get; set; }
+
+        public void infoC()
+        {
+            Console.WriteLine("El Nombre del Padre es: " + NombrePapa);
+            Console.WriteLine("El Nombre de la Madre es: " + NombreMama);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseC C = new ClaseC();
+
+            Console.WriteLine("Escriba su Nombre: ");
+            C.nombre = Console.ReadLine();
+            Console.WriteLine("Escriba su Apellido: ");
+            C.apellido = Console.ReadLine();
+            Console.WriteLine("Escriba su Edad: ");
+            C.edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba su Sueldo: ");
+            C.sueldo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba el Nombre del Padre: ");
+            C.NombrePapa = Console.ReadLine();
+            Console.WriteLine("Escriba el Nombre de la Madre: ");
+            C.NombreMama = Console.ReadLine();
+            
+            Console.Clear();
+            C.infoA();
+            C.infoB();
+            C.infoC();
+            Console.ReadKey();
+        }
+    }
+}
