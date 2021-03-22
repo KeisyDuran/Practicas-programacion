88f68f46eb25e84d5e9ab50b8a899e36687f1e32
Keisy.Duran <77956724+KeisyDuran@users.noreply.github.com>
2021-03-22 18:48:30 -0400

    .

--- practica06/ejercicio2/Program.cs
+++ practica06/ejercicio2/Program.cs
@@ -2,11 +2,44 @@
 
 namespace ejercicio2
 {
+    class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+
+        public void setContacto()
+        {
+            Console.WriteLine( Nombre +" "+ Apellido );
+          
+        }
+        public void saludar()
+        {
+            Console.WriteLine("hola, soy "+ Nombre+" "+ Apellido+" Mi Numero de telefono es "+ Telefono+" Direccion "+ Direccion);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Contacto con = new Contacto();
+
+            Console.WriteLine("Escriba Su Nombre");
+            con.Nombre = Console.ReadLine();
+            Console.WriteLine("Escriba Su Apellido");
+            con.Apellido = Console.ReadLine();
+            Console.WriteLine("Escriba Su Telefono");
+            con.Telefono = Console.ReadLine();
+            Console.WriteLine("Escriba Su Direccion");
+            con.Direccion = Console.ReadLine();
+
+            Console.Clear();
+            con.setContacto();
+
+            con.saludar();
+            Console.ReadKey();
         }
     }
 }
