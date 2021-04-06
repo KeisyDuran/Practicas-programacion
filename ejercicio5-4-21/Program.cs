82ff6d7ce569c7773f24941547a4125823aae1c4
Keisy.Duran <77956724+KeisyDuran@users.noreply.github.com>
2021-04-05 20:19:46 -0400

    .

---
+++ ejercicio5-4-21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ejercicio5_4_21
+{
+    namespace productos
+  {
+    class Producto
+    {
+        public string Nombre;
+        public string Descripcion;
+        public int Cantidad;
+        public Producto(string nom, string desc, int cant)
+   {
+     this.Nombre = nom;
+     this.Descripcion = desc;
+     this.Cantidad = cant;
+    }
+        public void MonstrarProducto()
+   {
+    Console.WriteLine("Nombre producto: {0}" , this.Nombre);
+    Console.WriteLine("Descripcion: {0}" , this.Descripcion);
+
+   }
+
+   }
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto prod = new Producto (nom:"Cerveza Corona", desc:"Cerveza Mexicana", cant:12);
+            prod.MonstrarProducto();
+        }
+    }
+}
+  
+ }
