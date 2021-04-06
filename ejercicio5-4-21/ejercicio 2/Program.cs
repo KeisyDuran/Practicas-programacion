d034b9796ab1ee40dd284a629a5038287ce7b728
Keisy.Duran <77956724+KeisyDuran@users.noreply.github.com>
2021-04-05 20:44:19 -0400

    .

--- ejercicio5-4-21/ejercicio 2/Program.cs
+++ ejercicio5-4-21/ejercicio 2/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 
-namespace ejercicio_2
+namespace ConsoleApp1
 {
+    
     class Program
     {
+        //Crear aplicacion recibir menor y mayor entero de un vector. Recibirlos en un solo metodo y retornar los valores.
+        
+        private int[] vect;
+
+        public Program()
+        {
+            Console.Write("Tamaño del vectro: ");
+            int tam = int.Parse(Console.ReadLine());
+            vect = new int[tam];
+        }
+
+        public void Cargar()
+        {
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.Write("Ingresar valor");
+                vect[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void MenorMayor(out int mayor, out int menor)
+        {
+            mayor = vect[0];
+            menor = vect[0];
+
+            for (int i = 1; i < vect.Length; i++)
+            {
+                if (vect[i] > mayor)
+                {
+                    mayor = vect[i];
+                }
+                else
+                {
+                    if (vect[i] < menor)
+                    {
+                        menor = vect[i];
+                    }
+                }
+                
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            
+            p.Cargar();
+
+            int may, men;
+            
+            p.MenorMayor(out may, out men);
+            Console.WriteLine("El elemento mayor de la tabla es: {0}", may);
+            Console.WriteLine("El elemento menor de la tabla es: {0}", men);
         }
     }
 }
---
+++ ejercicio5-4-21/ejercicio 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ejercicio_3
+{
+    class Program
+    {
+        public void ValoresAleatorios(out int v1, out int v2, out int v3, out int v4, out int v5)
+        {
+            Random r = new Random();
+            v1 = r.Next(1, 30);
+            v2 = r.Next(1, 30); 
+            v3 = r.Next(1, 30);
+            v4 = r.Next(1, 30);
+            v5 = r.Next(1, 30);           
+        }
+
+        static void Main(string[] args)
+        {
+            int ale1, ale2, ale3, ale4, ale5;
+            Program K = new Program();
+            K.ValoresAleatorios(out ale1, out ale2, out ale3, out ale4, out ale5);
+            Console.WriteLine("Primer valor aleatorio:" + ale1);
+            Console.WriteLine("Segundo valor aleatorio:" + ale2);
+            Console.WriteLine("Segundo valor aleatorio:" + ale3);
+            Console.WriteLine("Segundo valor aleatorio:" + ale4);
+            Console.WriteLine("Segundo valor aleatorio:" + ale5);  
+            Console.ReadKey();
+        }
+    }
+}
