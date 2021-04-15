1cee1267c86e6383367af1f68486f56d4e97b9b9
Keisy.Duran <77956724+KeisyDuran@users.noreply.github.com>
2021-04-14 23:14:13 -0400

    .

--- Proyecto Final/Program.cs
+++ Proyecto Final/Program.cs
@@ -6,21 +6,6 @@ namespace Proyecto_Final
     {
         static void Main(string[] args)
         {
-            float pago, Interes_pagado, Capital_pagado, Monto_del_prestamo, tasa_interes_anual, tasa_interes_mensual;
-            int  fila, Plazo, i, Beneficiario;
-           
-
-          Console.Write("\n Bienvenidos al Simulador de Préstamos\n");
-          Console.Write("Favor Introduzca las siguientes informaciones\n");
-          Console.Write("Introduce el Beneficiario: ");
-          Beneficiario.ToString(Console.ReadLine());
-          Console.Write("Introduce el monto del prestamo: ");
-          float.TryParse(Console.ReadLine(), out Monto_del_prestamo);
-          Console.Write("Introduce la tasa de interes anual: ");
-          float.TryParse(Console.ReadLine(), out tasa_interes_anual);
-          Console.Write("Introduce el plazo en meses: ");
-          int.TryParse(Console.ReadLine(), out Plazo);
-
 
 
         }
